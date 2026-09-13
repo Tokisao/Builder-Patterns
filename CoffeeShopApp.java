f95fc39aba3dcdaf5c6751b657a622d864c7d30c
Dayana <252830@astanait.edu.kz>
2026-09-13 20:55:43 +0500

    Add comment to main method in CoffeeShopApp
    Added comment to clarify the role of the director and builders.

--- CoffeeShopApp.java
+++ CoffeeShopApp.java
@@ -1,6 +1,7 @@
 public class CoffeeShopApp {
     public static void main(String[] args) {
         BaristaDirector director = new BaristaDirector();
+        // Director defines recipes; builders enforce each drink's invariants
         System.out.println(director.makeEspresso(new EspressoBuilder()));
         System.out.println(director.makeLatte(new LatteBuilder()));
     }
