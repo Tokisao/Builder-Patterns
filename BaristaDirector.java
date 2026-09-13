d4962279e47bdccdf2869614f7adb3d787e3d88a
Dayana <252830@astanait.edu.kz>
2026-09-13 20:55:11 +0500

    Add comments to coffee preparation methods
    Added comments to clarify espresso and latte preparation.

--- BaristaDirector.java
+++ BaristaDirector.java
@@ -1,8 +1,11 @@
 public class BaristaDirector {
     public Coffee makeEspresso(CoffeeBuilder b) {
+        // Espresso canon: hot, double shot, no milk
         return b.temperature("hot").extraShots(2).build();
     }
+
     public Coffee makeLatte(CoffeeBuilder b) {
+        // Default latte on oat milk, single shot
         return b.setMilk("oat").temperature("hot").extraShots(1).build();
     }
 }
